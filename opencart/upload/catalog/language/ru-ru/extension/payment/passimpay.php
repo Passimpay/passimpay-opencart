c5c17187598cd528aaa2d5eac2857c07ab6eeb50
Passimpay <118345040+Passimpay@users.noreply.github.com>
2026-03-24 01:21:16 +0000

    Update passimpay.php

--- opencart/upload/catalog/language/ru-ru/extension/payment/passimpay.php
+++ opencart/upload/catalog/language/ru-ru/extension/payment/passimpay.php
@@ -1,4 +1,7 @@
 <?php
 // Text
-$_['order_description'] = 'Оплата заказа #';
-?>
+$_['text_title']           = 'Passimpay';
+$_['text_title_both']      = 'Passimpay (карта и криптовалюта)';
+$_['text_title_crypto']    = 'Passimpay (только криптовалюта)';
+$_['text_title_card']      = 'Passimpay (только банковская карта)';
+$_['text_redirect_notice'] = 'Вы будете перенаправлены на защищённую страницу оплаты.';
