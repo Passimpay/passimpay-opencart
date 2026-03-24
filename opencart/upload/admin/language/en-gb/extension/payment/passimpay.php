ccebd10b057799c896697c508f5277d24d98f835
Passimpay <118345040+Passimpay@users.noreply.github.com>
2026-03-24 01:15:58 +0000

    Update passimpay.php

--- opencart/upload/admin/language/en-gb/extension/payment/passimpay.php
+++ opencart/upload/admin/language/en-gb/extension/payment/passimpay.php
@@ -1,29 +1,41 @@
 <?php
 // Heading
-$_['heading_title'] = 'Passimpay';
+$_['heading_title']    = 'Passimpay';
 
-// Text 
-$_['text_edit'] = 'Edit module';
-$_['text_payment'] = 'Payment';
-$_['text_passimpay'] = '<a href="https://passimpay.io/" target="_blank"><img width="100" height="31" src="view/image/payment/passim_logo_en.svg" alt="Passimpay" title="Passimpay" style="border: 1px solid #EEEEEE;" /></a>';
-$_['text_success'] = 'Settings updated';   
-$_['text_pay'] = 'Passimpay';
-$_['text_response_description'] = 'Error text:';
-$_['text_enot_order_status'] = 'Order status:';
-$_['text_response_code'] = 'Error code:';
+// Text
+$_['text_home']        = 'Home';
+$_['text_extension']   = 'Extensions';
+$_['text_all_zones']   = 'All Zones';
+$_['text_success']     = 'Success: You have modified Passimpay payment module!';
+$_['text_edit']        = 'Edit Passimpay';
+$_['text_enabled']     = 'Enabled';
+$_['text_disabled']    = 'Disabled';
 
 // Entry
-$_['entry_merchant_id'] = 'Shop ID:';
-$_['entry_apikey'] = 'ApiKey:';
-$_['entry_title'] = 'Title:';
+$_['entry_api_key']    = 'API Key (Secret Key)';
+$_['entry_platform_id'] = 'Platform ID';
+$_['entry_payment_type'] = 'Payment options';
+$_['entry_payment_type_both']   = 'Card and cryptocurrency';
+$_['entry_payment_type_crypto'] = 'Cryptocurrency only';
+$_['entry_payment_type_card']   = 'Bank card only';
+$_['entry_card_notice'] = 'Before enabling card payments, ensure that «Cards/Bank transfer» is turned on in your Passimpay platform settings.';
+$_['entry_order_status_success_id'] = 'Order Status (paid)';
+$_['entry_order_status_pending_id'] = 'Order Status (pending)';
+$_['entry_order_status_failed_id']   = 'Order Status (failed/cancelled)';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+$_['entry_geo_zone']   = 'Geo Zone';
+$_['entry_callback_url'] = 'URL for notifications (callback)';
+$_['entry_return_url'] = 'Return URL';
 
-$_['entry_order_status'] = 'Order status after payment:';
+// Tab
+$_['tab_general']      = 'General';
 
-$_['entry_status'] = 'Status:';
-$_['entry_sort_order'] = 'Ordering:';
+// Button
+$_['button_save']      = 'Save';
+$_['button_cancel']    = 'Cancel';
 
 // Error
-$_['error_permission'] = "You haven't permission!";
-$_['error_merchant_id'] = 'Shop ID is empty!';
-$_['error_apikey'] = 'ApiKey is empty!';
-?>
+$_['error_permission'] = 'Warning: You do not have permission to modify payment Passimpay!';
+$_['error_api_key']    = 'API Key required';
+$_['error_platform_id'] = 'Platform ID required';
