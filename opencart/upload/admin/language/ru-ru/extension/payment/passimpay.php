604e4604a3254e697ac0a0ed7d0af0c439d64df9
Passimpay <118345040+Passimpay@users.noreply.github.com>
2026-05-19 17:03:00 +0100

    Update Russian language file for Passimpay module

--- opencart/upload/admin/language/ru-ru/extension/payment/passimpay.php
+++ opencart/upload/admin/language/ru-ru/extension/payment/passimpay.php
@@ -1,8 +1,6 @@
 <?php
-// Heading
 $_['heading_title']    = 'Passimpay';
 
-// Text
 $_['text_home']        = 'Главная';
 $_['text_extension']   = 'Расширения';
 $_['text_all_zones']   = 'Все зоны';
@@ -10,32 +8,34 @@ $_['text_success']     = 'Настройки модуля Passimpay обновл
 $_['text_edit']        = 'Редактировать Passimpay';
 $_['text_enabled']     = 'Включено';
 $_['text_disabled']    = 'Выключено';
+$_['text_yes']         = 'Да';
+$_['text_no']          = 'Нет';
+$_['text_check_status_hint'] = 'Введите ID заказа, чтобы вручную запросить его статус у Passimpay. Используется, если webhook не пришёл.';
 
-// Entry
-$_['entry_api_key']    = 'API ключ (Secret Key)';
-$_['entry_platform_id'] = 'Platform ID';
-$_['entry_payment_type'] = 'Способ оплаты';
-$_['entry_payment_type_both']   = 'Карта и криптовалюта';
-$_['entry_payment_type_crypto'] = 'Только криптовалюта';
-$_['entry_payment_type_card']   = 'Только банковская карта';
-$_['entry_card_notice'] = 'Перед включением оплаты картой убедитесь, что в настройках платформы в Passimpay включён переключатель «Карты/банковский перевод».';
-$_['entry_order_status_success_id'] = 'Статус заказа (оплачен)';
-$_['entry_order_status_pending_id'] = 'Статус заказа (ожидание)';
-$_['entry_order_status_failed_id']   = 'Статус заказа (ошибка/отмена)';
-$_['entry_status']     = 'Статус';
-$_['entry_sort_order'] = 'Порядок сортировки';
-$_['entry_geo_zone']   = 'Геозона';
-$_['entry_callback_url'] = 'URL для уведомлений (callback)';
-$_['entry_return_url'] = 'URL возврата';
+$_['entry_api_key']              = 'API ключ (Secret Key)';
+$_['entry_platform_id']          = 'Platform ID';
+$_['entry_payment_type']         = 'Способ оплаты';
+$_['entry_payment_type_both']    = 'Карта и криптовалюта';
+$_['entry_payment_type_crypto']  = 'Только криптовалюта';
+$_['entry_payment_type_card']    = 'Только банковская карта';
+$_['entry_card_notice']          = 'Перед включением оплаты картой убедитесь, что в настройках платформы Passimpay включён переключатель «Карты/банковский перевод».';
+$_['entry_order_status_success'] = 'Статус заказа (оплачен)';
+$_['entry_order_status_pending'] = 'Статус заказа (ожидание)';
+$_['entry_order_status_failed']  = 'Статус заказа (ошибка/отмена)';
+$_['entry_status']               = 'Статус';
+$_['entry_sort_order']           = 'Порядок сортировки';
+$_['entry_geo_zone']             = 'Геозона';
+$_['entry_callback_url']         = 'URL для уведомлений (callback)';
+$_['entry_return_url']           = 'URL возврата';
+$_['entry_check_status_order_id'] = 'ID заказа';
 
-// Tab
 $_['tab_general']      = 'Основное';
+$_['tab_tools']        = 'Инструменты';
 
-// Button
-$_['button_save']      = 'Сохранить';
-$_['button_cancel']    = 'Отмена';
+$_['button_save']         = 'Сохранить';
+$_['button_cancel']       = 'Отмена';
+$_['button_check_status'] = 'Проверить статус';
 
-// Error
-$_['error_permission'] = 'Недостаточно прав для изменения модуля Passimpay.';
-$_['error_api_key']    = 'Укажите API ключ';
+$_['error_permission']  = 'Недостаточно прав для изменения модуля Passimpay.';
+$_['error_api_key']     = 'Укажите API ключ';
 $_['error_platform_id'] = 'Укажите Platform ID';
