3c460b66ca2742a2cf9184cc136b03ed34ad8485
Passimpay <118345040+Passimpay@users.noreply.github.com>
2026-05-19 17:05:48 +0100

    Add loading text and confirm button label

--- opencart/upload/catalog/language/en-gb/extension/payment/passimpay.php
+++ opencart/upload/catalog/language/en-gb/extension/payment/passimpay.php
@@ -1,7 +1,9 @@
 <?php
-// Text
 $_['text_title']           = 'Passimpay';
 $_['text_title_both']      = 'Passimpay (Card and cryptocurrency)';
 $_['text_title_crypto']    = 'Passimpay (Cryptocurrency only)';
 $_['text_title_card']      = 'Passimpay (Bank card only)';
 $_['text_redirect_notice'] = 'You will be redirected to the secure payment page.';
+$_['text_loading']         = 'Loading...';
+
+$_['button_confirm']       = 'Confirm Order';
