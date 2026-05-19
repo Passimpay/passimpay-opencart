7415d65e1506d84a83f56128dc908c4f1b95d532
Passimpay <118345040+Passimpay@users.noreply.github.com>
2026-05-19 17:06:27 +0100

    Refactor Passimpay payment method configuration

--- opencart/upload/catalog/model/extension/payment/passimpay.php
+++ opencart/upload/catalog/model/extension/payment/passimpay.php
@@ -1,34 +1,40 @@
 <?php
 /**
- * Passimpay payment method - Catalog Model
+ * Passimpay payment method - Catalog Model (ocStore 2.3.x)
+ *
+ * Note: in ocStore 2.3 model is loaded by checkout controller as
+ *       $this->load->model('extension/payment/passimpay'), so the file
+ *       lives in catalog/model/extension/payment/.
  */
 class ModelExtensionPaymentPassimpay extends Model {
     public function getMethod($address, $total) {
         $this->load->language('extension/payment/passimpay');
-        $status = $this->config->get('payment_passimpay_status');
-        if (!$status) {
-            return [];
+
+        if (!$this->config->get('passimpay_status')) {
+            return array();
         }
-        $geo_zone_id = (int) $this->config->get('payment_passimpay_geo_zone_id');
+
+        $geo_zone_id = (int)$this->config->get('passimpay_geo_zone_id');
         if ($geo_zone_id) {
-            $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int) $geo_zone_id . "' AND country_id = '" . (int) $address['country_id'] . "' AND (zone_id = '" . (int) $address['zone_id'] . "' OR zone_id = '0')");
-            if (!$query->num_rows) {
-                return [];
+            $q = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$geo_zone_id . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+            if (!$q->num_rows) {
+                return array();
             }
         }
-        $payment_type = (int) $this->config->get('payment_passimpay_payment_type');
-        $titles = [
+
+        $payment_type = (int)$this->config->get('passimpay_payment_type');
+        $titles = array(
             0 => $this->language->get('text_title_both'),
             1 => $this->language->get('text_title_crypto'),
             2 => $this->language->get('text_title_card'),
-        ];
-        $title = $titles[$payment_type] ?? $this->language->get('text_title');
-        $sort_order = (int) $this->config->get('payment_passimpay_sort_order');
-        return [
+        );
+        $title = isset($titles[$payment_type]) ? $titles[$payment_type] : $this->language->get('text_title');
+
+        return array(
             'code'       => 'passimpay',
             'title'      => $title,
             'terms'      => $this->language->get('text_redirect_notice'),
-            'sort_order' => $sort_order,
-        ];
+            'sort_order' => (int)$this->config->get('passimpay_sort_order'),
+        );
     }
 }
